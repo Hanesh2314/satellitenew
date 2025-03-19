f911158abc6b4d08b86f16643288e5296e71fa5c
Hanesh2314 <71438546+Hanesh2314@users.noreply.github.com>
2025-03-20 01:56:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  root: './client',
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,7 +11,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: './dist',
+    emptyOutDir: true
   }
 })
